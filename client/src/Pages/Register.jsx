4e5cda65a87b324b8ac24f96bbc4025d49cc3eed
am0207522 <am0207522@gmail.com>
2026-06-14 23:39:49 +0530

    feat: implement Login component with form handling and validation

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -1,13 +1,11 @@
-import React from "react";
+import React from 'react'
 
 const Register = () => {
   return (
     <>
-      <div className="bg-green-900 text-white p-4">
-        Register
-      </div>
+       <div>Register</div>
     </>
-  );
-};
+  )
+}
 
-export default Register;
+export default Register
