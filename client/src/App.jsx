68d220f9f4033b81abef69fee95d9bc5393f2edd
am0207522 <am0207522@gmail.com>
2026-06-14 23:28:39 +0530

    feat: update App component structure and add CSS variables

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import ContactUs from "./Pages/ContactUs";
+import Navbar from "./components/Navbar";
+
 
 const App = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+
+      </Routes>
+    </BrowserRouter>
   );
 };
+
 
 export default App;
