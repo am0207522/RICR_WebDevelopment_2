aa89dac189bba2561456b4da2f122600674563a8
am0207522 <am0207522@gmail.com>
2026-07-08 20:53:04 +0530

    style: update WishList component text for consistency

--- client/src/components/userDashboard/WishList.jsx
+++ client/src/components/userDashboard/WishList.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const WishList = () => {
   return (
     <div className="min-h-screen flex items-center justify-center text-3xl font-bold">
-      WishList
+      WishList.
     </div>
   );
 };
