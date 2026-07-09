a62118c84ce2cc4e2f9b3829b127e87bdc597e7d
am0207522 <am0207522@gmail.com>
2026-07-09 19:41:17 +0530

    style: add CustomerDashboard and RestaurantDashboard components with access control

---
+++ client/src/Pages/dashboard/RestaurantDashboard.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+import RestaurantSidebar from "../../components/restaurantDashboard/RestaurantSidebar";
+import RestaurantOverview from "../../components/restaurantDashboard/RestaurantOverview";
+import RestaurantSetting from "../../components/restaurantDashboard/RestaurantSetting";
+import RestaurantOrders from "../../components/restaurantDashboard/RestaurantOrders";
+
+const RestaurantDashboard = () => {
+  const { isLogin, role } = useAuth();
+  const navigate = useNavigate();
+  const active = useLocation().state?.activeTab;
+  const [activeTab, setActiveTab] = React.useState(active || "overview");
+
+  if (!isLogin || role !== "restaurant") {
+    return (
+      <div className="h-[92vh] bg-[url('/foodTable.webp')]  bg-cover bg-center">
+        <div className="h-full backdrop-blur-lg flex flex-col items-center justify-center ">
+          <h1 className="text-2xl font-bold text-(--color-neutral-content)">
+            Access Denied. Please log in as a Restaurant Manager to view this
+            page.
+          </h1>
+          <button
+            className="mt-4 px-4 py-2 bg-(--color-primary) text-white rounded-md"
+            onClick={() => navigate("/login")}
+          >
+            Go to Login
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <div className="flex h-[92vh]">
+        <div className="w-1/6 border border-red-500 h-full">
+          <Sidebar active={active} setActive={setActive} />
+        </div>
+        <div className="w-5/6 border border-green-500 h-full P-3">
+          {active === "Overview" && <RestaurantOverview />}
+          {active === "Orders" && <RestaurantOrders />}
+          {active === "Settings" && <RestaurantSetting />}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default RestaurantDashboard;
