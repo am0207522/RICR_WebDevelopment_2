e4733b36a75fa537c670eb4b31ceaa61c391d79d
am0207522 <am0207522@gmail.com>
2026-07-12 08:08:58 +0530

    style: reorganize imports and wrap App component with BrowserRouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import ContactUs from "./pages/ContactUs";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import CustomerDashboard from "./pages/dashboard/CustomerDashboard";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import CustomerDashboard from "./pages/dashboard/CustomerDashboard";
+import ContactUs from "./pages/ContactUs";
 import RestaurantDashboard from "./pages/dashboard/RestaurantDashboard";
 import RiderDashboard from "./pages/dashboard/RiderDashboard";
 import AdminDashboard from "./pages/dashboard/AdminDashboard";
@@ -15,21 +15,21 @@ import AdminDashboard from "./pages/dashboard/AdminDashboard";
 const App = () => {
   return (
     <>
-    <BrowserRouter>
       <Toaster />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/contact-us" element={<ContactUs />} />        
+
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/register/:userType" element={<Register />} />
         <Route path="/customer-dashboard" element={<CustomerDashboard />} />      {/* dashborad routes */} 
         <Route path="/restaurant-dashboard" element={<RestaurantDashboard />} />
         <Route path="/rider-dashboard" element={<RiderDashboard />} />
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
       </Routes>
       <Footer/>
-    </BrowserRouter>
     </>
   );
 };
