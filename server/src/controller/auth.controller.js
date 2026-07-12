65d2e29749f54982dbc0bbde365a3c322257d2b0
am0207522 <am0207522@gmail.com>
2026-07-12 08:10:02 +0530

    feat: implement user registration, OTP verification, and password reset functionality; add user profile editing and password update routes; remove unused contact routes

--- server/src/controller/auth.controller.js
+++ server/src/controller/auth.controller.js
@@ -1,6 +1,10 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import { genToken } from "../utils/auth.service.js";
+import OTP from "../models/otp.model.js";
+
+import { genOTPToken } from "../utils/auth.service.js";
+import { sendOTPEmail } from "../utils/email.service.js";
 
 export const RegisterUser = async (req, res, next) => {
   try {
@@ -8,8 +12,15 @@ export const RegisterUser = async (req, res, next) => {
       req.body;
 
     if (
-      !fullName || !email || !password || !phone || !gender || !dob || !userType) {
-    const error = new Error("All fields Required");
+      !fullName ||
+      !email ||
+      !password ||
+      !phone ||
+      !gender ||
+      !dob ||
+      !userType
+    ) {
+      const error = new Error("All fields Required");
       error.statusCode = 400;
       return next(error);
     }
@@ -21,14 +32,12 @@ export const RegisterUser = async (req, res, next) => {
       return next(error);
     }
 
-    // photo ek simple string hai (object nahi)
     const photoURL = `https://placehold.co/600x400?text=${fullName.charAt(0).toUpperCase()}`;
 
     const photo = {
       url: photoURL,
       publicId: null,
     };
-
     const SALT = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, SALT);
 
@@ -54,14 +63,12 @@ export const LoginUser = async (req, res, next) => {
   try {
     const { email, password } = req.body;
 
-    // check login pass
     if (!email || !password) {
       const error = new Error("All fields Required");
       error.statusCode = 400;
       return next(error);
     }
 
-    //check in database user ?
     const existingUser = await User.findOne({ email });
     if (!existingUser) {
       const error = new Error("Email not registred");
@@ -69,7 +76,6 @@ export const LoginUser = async (req, res, next) => {
       return next(error);
     }
 
-    // verify user with password
     const isVerified = await bcrypt.compare(password, existingUser.password);
     if (!isVerified) {
       const error = new Error("Incorrect Password");
@@ -77,10 +83,8 @@ export const LoginUser = async (req, res, next) => {
       return next(error);
     }
 
-    //genrate Token
     await genToken(existingUser, res);
 
-    // then log succesfully welcome back!
     res.status(200).json({
       message: "Welcome Back",
       data: existingUser,
@@ -93,8 +97,110 @@ export const LoginUser = async (req, res, next) => {
 
 export const LogoutUser = async (req, res, next) => {
   try {
-    res.clearCookie("Cravings", { maxAge: 0 }); //Controller Logic -
+    res.clearCookie("Oreo", { maxAge: 0 });
+
     res.status(200).json({ message: "Logout Sucessfully" });
+  } catch (error) {
+    console.log(error.message);
+    next();
+  }
+};
+
+export const SendOtp = async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    if (!email) {
+      const error = new Error("Email is required");
+      error.statusCode = 400;
+      return next(error);
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      const error = new Error("Email not registered");
+      error.statusCode = 404;
+      return next(error);
+    }
+
+    // Generate and send OTP here
+    const newOTP = (Math.floor(Math.random() * 1000000) + 100000)
+      .toString()
+      .slice(0, 6);
+
+    //Send OTP via Email
+    const hashedOTP = await bcrypt.hash(newOTP, 10);
+    const existingOTP = await OTP.findOne({ email });
+    if (existingOTP) {
+      await existingOTP.deleteOne();
+    }
+
+    const saveOTP = await OTP.create({
+      email,
+      otp: hashedOTP,
+    });
+    await sendOTPEmail(email, newOTP);
+
+    res.status(200).json({ message: `OTP sent on '${email}'` });
+  } catch (error) {
+    console.log(error.message);
+    next();
+  }
+};
+export const VerifyOtp = async (req, res, next) => {
+  try {
+    const { email, otp } = req.body;
+
+    if (!email) {
+      const error = new Error("Email is required");
+      error.statusCode = 400;
+      return next(error);
+    }
+
+    const existingOTP = await OTP.findOne({ email });
+    if (!existingOTP) {
+      const error = new Error("OTP Expired");
+      const statusCode = 401;
+      return next(error);
+    }
+
+    const isVerified = await bcrypt.compare(otp, existingOTP.otp);
+    if (!isVerified) {
+      const error = new Error("OTP Expired");
+      const statusCode = 401;
+      return next(error);
+    }
+
+    await existingOTP.deleteOne();
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      const error = new Error("Email not registered");
+      error.statusCode = 404;
+      return next(error);
+    }
+
+    await genOTPToken(existingUser, res);
+    res
+      .status(200)
+      .json({ message: "OTP verified. Create You New Password Now" });
+  } catch (error) {
+    console.log(error.message);
+    next();
+  }
+};
+export const ResetPassword = async (req, res, next) => {
+  try {
+    const { newPassword } = req.body;
+
+    const currentUser = req.user;
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+    currentUser.password = hashedPassword;
+
+    await currentUser.save();
+
+    res.status(200).json({ message: "Password Changed" });
   } catch (error) {
     console.log(error.message);
     next();
--- server/src/router/contact.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { contactUser } from "../controller/contact.controller.js";
-
-const router = express.Router();
-
-router.post("/", contactUser);
-
-export default router;
