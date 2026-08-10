9f8bb7e0abd8debf796da61695840e1e497d68ce
am0207522 <am0207522@gmail.com>
2026-08-10 23:50:41 +0530

    chore: add GetAllRestaurants and GetRestaurantDetail routes to public API

--- server/src/router/public.route.js
+++ server/src/router/public.route.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { ContactUsForm } from "../controller/public.controller.js";
+import {
+  ContactUsForm,
+  GetAllRestaurants,
+  GetRestaurantDetail,
+} from "../controller/public.controller.js";
 
 const router = express.Router();
 
 router.post("/contact-us", ContactUsForm);
+router.get("/restaurants", GetAllRestaurants);
+router.get("/restaurant-detail/:restaurantId", GetRestaurantDetail);
 
 
 export default router;
